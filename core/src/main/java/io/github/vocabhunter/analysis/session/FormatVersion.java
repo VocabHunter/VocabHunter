8de44c6b11f36a691e3c5b13e061703de6def7d8
Adam Carroll <AdamCarroll@users.noreply.github.com>
2016-03-03 23:47:44 +0100

    Added licence information

--- core/src/main/java/io/github/vocabhunter/analysis/session/FormatVersion.java
+++ core/src/main/java/io/github/vocabhunter/analysis/session/FormatVersion.java
@@ -1,3 +1,7 @@
+/*
+ * Open Source Software published under the Apache Licence, Version 2.0.
+ */
+
 package io.github.vocabhunter.analysis.session;
 
 public final class FormatVersion {
--- osx/src/osx/java/io/github/vocabhunter/osx/OsxGuiContainerBuilder.java
+++ osx/src/osx/java/io/github/vocabhunter/osx/OsxGuiContainerBuilder.java
@@ -1,3 +1,7 @@
+/*
+ * Open Source Software published under the Apache Licence, Version 2.0.
+ */
+
 package io.github.vocabhunter.osx;
 
 import io.github.vocabhunter.gui.container.GuiContainerBuilder;
