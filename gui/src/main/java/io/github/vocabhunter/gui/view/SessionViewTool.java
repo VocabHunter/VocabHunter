4fb0a37845dcdd84dbb516dd36c047d15e33c8d8
Adam Carroll <AdamCarroll@users.noreply.github.com>
2016-05-04 23:33:51 +0200

    Added tabs to the session display

---
+++ gui/src/main/java/io/github/vocabhunter/gui/view/SessionViewTool.java
@@ -0,0 +1,30 @@
+/*
+ * Open Source Software published under the Apache Licence, Version 2.0.
+ */
+
+package io.github.vocabhunter.gui.view;
+
+import javafx.scene.Node;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
+public class SessionViewTool {
+    private final Tab analysisTab = new Tab("Text Analysis");
+
+    private final Tab progressTab = new Tab("Progress Report");
+
+    private final TabPane tabPane = new TabPane(analysisTab, progressTab);
+
+    public SessionViewTool() {
+        analysisTab.setClosable(false);
+        progressTab.setClosable(false);
+    }
+
+    public void addAnalysisView(final Node view) {
+        analysisTab.setContent(view);
+    }
+
+    public Node getView() {
+        return tabPane;
+    }
+}
