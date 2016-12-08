4dda6a2a101d3eea8bcaa36e4e8ecda7be3491fd
Adam Carroll <AdamCarroll@users.noreply.github.com>
2016-12-08 13:50:56 +0100

    Updated to Mockito 2.2.29

--- gui/src/test/java/io/github/vocabhunter/gui/main/GuiTest.java
+++ gui/src/test/java/io/github/vocabhunter/gui/main/GuiTest.java
@@ -33,7 +33,7 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.testfx.api.FxRobot;
 
 import java.io.IOException;
