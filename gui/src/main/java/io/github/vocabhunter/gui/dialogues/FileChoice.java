91f25e4a2faf4e66d86a571097dfd3c4f1142872
Adam Carroll <AdamCarroll@users.noreply.github.com>
2017-03-14 22:44:01 +0100

    Improved testing of file handling

--- gui/src/main/java/io/github/vocabhunter/gui/dialogues/FileChoice.java
+++ gui/src/main/java/io/github/vocabhunter/gui/dialogues/FileChoice.java
@@ -4,6 +4,10 @@
 
 package io.github.vocabhunter.gui.dialogues;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 import java.nio.file.Path;
 
 public final class FileChoice {
@@ -22,5 +26,39 @@ public final class FileChoice {
 
     public FileFormatType getType() {
         return type;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        FileChoice that = (FileChoice) o;
+
+        return new EqualsBuilder()
+            .append(file, that.file)
+            .append(type, that.type)
+            .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+            .append(file)
+            .append(type)
+            .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+            .append("file", file)
+            .append("type", type)
+            .toString();
     }
 }
