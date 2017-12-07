342ca170906f24e2d6349de12f89b796185a5865
Adam Carroll <AdamCarroll@users.noreply.github.com>
2017-12-07 21:42:01 +0100

    Simplified opening files on system start

--- core/src/test/java/io/github/vocabhunter/analysis/session/FileNameToolTest.java
+++ core/src/test/java/io/github/vocabhunter/analysis/session/FileNameToolTest.java
@@ -6,6 +6,8 @@ package io.github.vocabhunter.analysis.session;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -39,12 +41,24 @@ public class FileNameToolTest {
         validate("/root/next/test", "/root/next/test.wordy");
     }
 
-
     private void validate(final String original, final String expected) {
         Path originalPath = Paths.get(original);
         Path expectedPath = Paths.get(expected);
 
         Path actual = FileNameTool.ensureSessionFileHasSuffix(originalPath);
         assertEquals(expectedPath, actual, "Path match");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "'test.wordy',       true",
+        "'/home/test.wordy', true",
+        "'/home/test',       false",
+        "'/home/test.txt',   false"
+    })
+    public void testIsSessionFile(final String name, final boolean expected) {
+        Path file = Paths.get(name);
+
+        assertEquals(expected, FileNameTool.isSessionFile(file));
     }
 }
