38da369167fa3c857e4655b70ed11dfd85f865e3
Adam Carroll <AdamCarroll@users.noreply.github.com>
2019-12-19 21:33:46 +0100

    Minor formatting cleanup

--- gui/src/main/java/io/github/vocabhunter/gui/dialogues/ErrorDialogue.java
+++ gui/src/main/java/io/github/vocabhunter/gui/dialogues/ErrorDialogue.java
@@ -44,7 +44,7 @@ public class ErrorDialogue {
 
     private void resizeAlert() {
         alert.getDialogPane().requestLayout();
-        
+
         Stage stage = (Stage)alert.getDialogPane().getScene().getWindow();
         stage.sizeToScene();
     }
