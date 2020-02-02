4c6fd5387b634f7f2f63c94d23ea67dc760ce403
Adam Carroll <AdamCarroll@users.noreply.github.com>
2020-02-02 12:23:26 +0100

    Further improved excel reading

--- core/src/main/java/io/github/vocabhunter/analysis/grid/ExcelGridReaderImpl.java
+++ core/src/main/java/io/github/vocabhunter/analysis/grid/ExcelGridReaderImpl.java
@@ -4,6 +4,7 @@
 
 package io.github.vocabhunter.analysis.grid;
 
+import io.github.vocabhunter.analysis.core.CoreConstants;
 import io.github.vocabhunter.analysis.core.VocabHunterException;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.usermodel.Row.MissingCellPolicy;
@@ -20,37 +21,36 @@ import static java.util.stream.Collectors.toList;
 
 @Singleton
 public class ExcelGridReaderImpl implements ExcelGridReader {
-    private final ThreadLocal<DataFormatter> dataFormatter = ThreadLocal.withInitial(DataFormatter::new);
-
     @Override
     public List<GridLine> readGrid(final Path file, final Predicate<String> filter) {
         try (Workbook workbook = WorkbookFactory.create(file.toFile())) {
             Sheet sheet = workbook.getSheetAt(0);
+            DataFormatter dataFormatter = new DataFormatter(CoreConstants.LOCALE);
 
             return StreamSupport.stream(sheet.spliterator(), false)
-                .map(r -> row(r, filter))
+                .map(r -> row(r, filter, dataFormatter))
                 .collect(toList());
         } catch (final IOException e) {
             throw new VocabHunterException("Unable to read spreadsheet " + file, e);
         }
     }
 
-    private GridLine row(final Row row, final Predicate<String> filter) {
+    private GridLine row(final Row row, final Predicate<String> filter, final DataFormatter dataFormatter) {
         List<GridCell> cells = IntStream.range(0, row.getLastCellNum())
             .mapToObj(i -> row.getCell(i, MissingCellPolicy.RETURN_BLANK_AS_NULL))
-            .map(c -> cell(c, filter))
+            .map(c -> cell(c, filter, dataFormatter))
             .collect(toList());
 
         return new GridLine(cells);
     }
 
-    private GridCell cell(final Cell original, final Predicate<String> filter) {
+    private GridCell cell(final Cell original, final Predicate<String> filter, final DataFormatter dataFormatter) {
         String text;
 
         if (original == null) {
             text = "";
         } else {
-            text = dataFormatter.get().formatCellValue(original).trim();
+            text = dataFormatter.formatCellValue(original).trim();
         }
 
         return new GridCell(text, filter.test(text));
