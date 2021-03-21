0700fa2a087876990e704a9694415060bee4ad45
Adam Carroll <AdamCarroll@users.noreply.github.com>
2021-03-21 15:46:31 +0100

    Started switch to JDK 16 jpackage tool

--- package/bin/build-package.bat
+++ package/bin/build-package.bat
@@ -1,24 +1,20 @@
-set MODULE_PATH=%1
-set INPUT=%2
-set OUTPUT=%3
-set JAR=%4
-set VERSION=%5
-set APP_ICON=%6
+set INSTALLER_TYPE=%1
+set MODULE_PATH=%2
+set INPUT=%3
+set OUTPUT=%4
+set JAR=%5
+set VERSION=%6
+set APP_ICON=%7
 
-call "%JAVA_HOME%\bin\java.exe" ^
-    -Xmx512M ^
-    --module-path "%JAVA_HOME%\jmods" ^
-    --add-opens jdk.jlink/jdk.tools.jlink.internal.packager=jdk.packager ^
-    -m jdk.packager/jdk.packager.Main ^
-    create-image ^
+call "jpackage" ^
+    --type "%INSTALLER_TYPE" ^
     --module-path "%MODULE_PATH%" ^
     --verbose ^
-    --echo-mode ^
     --add-modules "java.base,java.datatransfer,java.desktop,java.scripting,java.xml,jdk.jsobject,jdk.unsupported,jdk.unsupported.desktop,jdk.xml.dom,javafx.controls,javafx.fxml,java.naming,java.sql,jdk.charsets" ^
     --input "%INPUT%" ^
-    --output "%OUTPUT%" ^
+    --dest "%OUTPUT%" ^
     --name "VocabHunter" ^
     --main-jar "%JAR%" ^
-    --version "%VERSION%" ^
+    --app-version "%VERSION%" ^
     --icon "%APP_ICON%" ^
-    --class "io.github.vocabhunter.gui.main.VocabHunterGuiExecutable"
+    --main-class "io.github.vocabhunter.gui.main.VocabHunterGuiExecutable"
--- package/bin/build-package.sh
+++ package/bin/build-package.sh
@@ -14,17 +14,16 @@ APP_ICON=${9}
 EXTRA_BUNDLER_ARGUMENTS=${10}
 
 ${PACKAGER} \
-  create-installer ${INSTALLER_TYPE} \
+  --type ${INSTALLER_TYPE} \
   --module-path ${MODULE_PATH} \
   --verbose \
-  --echo-mode \
   --add-modules java.base,java.datatransfer,java.desktop,java.scripting,java.xml,jdk.jsobject,jdk.unsupported,jdk.unsupported.desktop,jdk.xml.dom,javafx.controls,javafx.fxml,java.naming,java.sql,jdk.charsets \
   --input "${INPUT}" \
-  --output "${OUTPUT}" \
+  --dest "${OUTPUT}" \
   --name VocabHunter \
   --main-jar ${JAR} \
-  --version ${VERSION} \
+  --app-version ${VERSION} \
   --file-associations ${FILE_ASSOCIATIONS} \
   --icon $APP_ICON \
   $EXTRA_BUNDLER_ARGUMENTS \
-  --class io.github.vocabhunter.gui.main.VocabHunterGuiExecutable
+  --main-class io.github.vocabhunter.gui.main.VocabHunterGuiExecutable
