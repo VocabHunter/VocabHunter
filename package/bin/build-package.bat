dca0f06153cf71d8a563dd352f0929546f6ff189
Adam Carroll <AdamCarroll@users.noreply.github.com>
2021-03-23 21:47:55 +0100

    Fixed a typo in the windows batch file

--- package/bin/build-package.bat
+++ package/bin/build-package.bat
@@ -7,7 +7,7 @@ set VERSION=%6
 set APP_ICON=%7
 
 call "jpackage" ^
-    --type "%INSTALLER_TYPE" ^
+    --type "%INSTALLER_TYPE%" ^
     --module-path "%MODULE_PATH%" ^
     --verbose ^
     --add-modules "java.base,java.datatransfer,java.desktop,java.scripting,java.xml,jdk.jsobject,jdk.unsupported,jdk.unsupported.desktop,jdk.xml.dom,javafx.controls,javafx.fxml,java.naming,java.sql,jdk.charsets" ^
