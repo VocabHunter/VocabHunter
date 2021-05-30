9e9f5f302cdd4508becf9a875118e48e6b6c9664
Adam Carroll <AdamCarroll@users.noreply.github.com>
2021-05-30 13:45:24 +0200

    Fixed problems with paths containing spaces in bundler script

--- package/bin/build-package.sh
+++ package/bin/build-package.sh
@@ -14,16 +14,16 @@ MODULES=${9}
 EXTRA_BUNDLER_ARGUMENTS=${10}
 
 jpackage \
-  --type ${INSTALLER_TYPE} \
-  --module-path ${MODULE_PATH} \
+  --type "${INSTALLER_TYPE}" \
+  --module-path "${MODULE_PATH}" \
   --verbose \
   --add-modules "${MODULES}" \
   --input "${INPUT}" \
   --dest "${OUTPUT}" \
   --name VocabHunter \
-  --main-jar ${JAR} \
-  --app-version ${VERSION} \
-  --file-associations ${FILE_ASSOCIATIONS} \
-  --icon $APP_ICON \
+  --main-jar "${JAR}" \
+  --app-version "${VERSION}" \
+  --file-associations "${FILE_ASSOCIATIONS}" \
+  --icon "$APP_ICON" \
   $EXTRA_BUNDLER_ARGUMENTS \
   --main-class io.github.vocabhunter.gui.main.VocabHunterGuiExecutable
